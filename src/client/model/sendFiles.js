3435353e3c86e436f0635bd3afa0b276a345b081
davidbaschung <davidbaschung@bluewin.ch>
2021-08-16 00:10:15 +0200

    corr

--- src/client/model/sendFiles.js
+++ src/client/model/sendFiles.js
@@ -68,7 +68,7 @@ function sendFileAsync(file) {
             console.log("recoveredBuffer : ",recoveredBuffer);
             while ( ! readyForSending ) await asyncSleep(100);
         }
-        await asyncSleep(50); // TODO keep?
+        // await asyncSleep(50); // TODO keep?
         while (senderDataChannel == null) {
             console.log("channel still null");
             await asyncSleep(10);
