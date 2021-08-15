e5805514c0ff1f30c8d7e821136f4acd6a3238e6
davidbaschung <davidbaschung@bluewin.ch>
2021-08-15 16:49:38 +0200

    while offset

--- src/client/model/sendFiles.js
+++ src/client/model/sendFiles.js
@@ -34,7 +34,7 @@ function sendFileAsync(file) {
         while (senderDataChannel == null) await asyncSleep(50);
         while (senderDataChannel.bufferedAmount + result.byteLength > MAXBUFFEREDAMOUNT)
             await asyncSleep(100); // TODO remove
-        while ( ! readyForSending && recoveredBuffer.length==0) { /* When the loading stream is interrupted by connection loss (through kill-switch) */
+        while ( ! readyForSending && recoveredBuffer.length==0 && offset!=0) { /* When the loading stream is interrupted by connection loss (through kill-switch) */
             const OFFSET_T0 = offset - senderDataChannel.bufferedAmount;
             console.log("Buffer Recovery activated.  offset:",offset," bufferdAmount:",senderDataChannel.bufferedAmount," OFFSET_T0:",OFFSET_T0);
             const RECOVERYAMOUNT = senderDataChannel.bufferedAmount;
