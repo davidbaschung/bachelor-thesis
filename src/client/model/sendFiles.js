225562de40fc1eaa2a626b5abaefbc15327fd121
davidbaschung <davidbaschung@bluewin.ch>
2021-08-22 01:31:29 +0200

    .size

--- src/client/model/sendFiles.js
+++ src/client/model/sendFiles.js
@@ -42,7 +42,7 @@ async function sendFileAsync(file) {
             var fileLookupIncrementingSize = 0;
             filesToSendCount = 0;
             while (fileLookupIncrementingSize + filesToSend[filesToSendCount].size <= securedSize) {
-                fileLookupIncrementingSize += filesToSend[filesToSendCount];
+                fileLookupIncrementingSize += filesToSend[filesToSendCount].size;
                 filesToSendCount++;
             }            
             offset = securedSize - fileLookupIncrementingSize;
